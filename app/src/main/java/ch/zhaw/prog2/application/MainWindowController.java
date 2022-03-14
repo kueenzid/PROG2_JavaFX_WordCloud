529d538d52c09ea55e4dc2d4b6f51367b4203e82
Dennis Küenzi <kueenden@students.zhaw.ch>
2022-03-14 16:17:55 +0100

    Schritt 6

---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindowController.java
@@ -0,0 +1,30 @@
+package ch.zhaw.prog2.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+
+    @FXML
+    private Label lableTitel;
+
+    @FXML
+    private TextField textEingabe;
+
+    @FXML
+    private TextArea textHistory;
+
+    @FXML
+    void hinzufuegenText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leerenTextEingabe(ActionEvent event) {
+
+    }
+
+}
